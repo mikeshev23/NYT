6a83b1b2da61c9efb98dd90aa54a544fbab7dd02
mike <mikeshev23@gmail.com>
2020-02-04 20:26:14 -0800

    added ajax to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-{\rtf1}
+var search = document.getElementById('submit')
+
+
+
+function displaySearch() {
+    var article = $(this).attr("data-name");
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=eIeGJ5ZOKIKObIKzOFis7zmGU1AICmgT"
+
+
+
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        
+        $('#articles').text(<h1> + response.response.docs.headline.main + </h1>)
+    })
+}
+
+function getArticles() {
+    var searchTerm = 'sanders'
+    var count = 0
+    var sYear = 0
+    var eYear = 0
+}
+
